2b8c57a09b7ef7bb5208ba8efa05dbed05e79f6d
Vil Rodriguez <vilmango@hotmail.com>
2020-01-29 17:04:41 -0500

    IsPathAbsolute test funciont jest

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//  Syntax for including Path module in your app
+let filePath = require('path');
+
+const isPathAbsolute = (filePath) =>{
+return console.log('meow');
+};
+
+
+const functions ={
+pathIsAbsolute : isPathAbsolute,
+
+};
+module.exports = functions;
+
+
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,2 +1,17 @@
+const functions = require('../src/index.js');
+// path.isAbsolute(myPath)
+// Test function Is the path absolute?
+// Input path = string  - Output Is absolute, true/false = boolean
 
-// Test here
+describe('pathIsAbsolute', () => {
+  it('Should be a function', () => {
+    expect(typeof functions.isPathAbsolute).toBe('function');
+  });
+
+  it('Should return true is the path is Absolute ', () => {
+    expect(functions.isPathAbsolute('/home/vilmango/Documents/LIM011-fe-md-links/README.md')).toBe(true);
+  });
+  it('Should return false if the path is Relative', () => {
+    expect(functions.isPathAbsolute('../README.md')).toBe(false);
+  });
+});
