04718e1c1f959d66ee3fcbfd29a3198ef55904bd
Vil Rodriguez <vilmango@hotmail.com>
2020-02-05 16:45:14 -0500

    deleted extra reasigned of function names on index for the import

--- src/index.js
+++ src/index.js
@@ -6,41 +6,33 @@ const isPathAbsolute = (filePath) => path.isAbsolute(filePath);
 const relativePathToAbsolute = (filePath) => path.resolve(filePath);
 const isAbsolutePathaFile = (filePath) => fs.lstatSync(filePath).isFile();
 const isAbsolutePathaFolder = (filePath) => fs.lstatSync(filePath).isDirectory();
-
 const verifyPathExtIsMD = (filePath) => (path.extname(filePath) === '.md');
 const getMDfilesFromArray = (fileArray) => fileArray.filter((element) => path.extname(element) === '.md');
-
 const getFilesInFolder = (filePath) => fs.readdirSync(filePath);
-// console.log(getFilesInFolder('/home/vilmango/Documents/LIM011-fe-md-links/prueba'));
 
-// 5 Received a path string and verify if there is  file or a folder, in case of folder
-// verifies if there're .md files, returns array
-const getMdFilesFromPath = (filePath) => {
+const getFileFromPathOrFolder = (filePath) => {
   let arrayMdFiles = [];
   if (isAbsolutePathaFile(filePath)) {
     arrayMdFiles.push(filePath);
   } else if (isAbsolutePathaFolder(filePath)) {
     getFilesInFolder(filePath).forEach((element) => {
-      arrayMdFiles = arrayMdFiles.concat(getMdFilesFromPath(path.join(filePath, element)));
+      arrayMdFiles = arrayMdFiles.concat(getFileFromPathOrFolder(path.join(filePath, element)));
     });
   }
   return arrayMdFiles;
 };
 
 
-const arreglo = getMdFilesFromPath('/home/vilmango/Documents/LIM011-fe-md-links/');
-// console.log(arreglo);
-// console.log(getMDfilesFromArray(arreglo));
-// console.log(getMdFilesFromPath('/home/vilmango/Documents/LIM011-fe-md-links/prueba'));
-// console.log(getMdFilesFromPath('/home/vilmango/Documents/LIM011-fe-md-links/prueba'));
+// console.log(isAbsolutePathaFolder('/home/vilmango/Documents/LIM011-fe-md-links/prueba/pato.html'));
+
 const functions = {
-  /* pathIsAbsolute:  */isPathAbsolute,
-  resolvePathToAbsolute: relativePathToAbsolute,
-  isPathaFile: isAbsolutePathaFile,
-  isPathaFolder: isAbsolutePathaFolder,
-  verifyFileIsMarkdown: verifyPathExtIsMD,
-  gettingFilesInFolder: getFilesInFolder,
-  getsMDFilesFromaPath: getMdFilesFromPath,
+  isPathAbsolute,
+  relativePathToAbsolute,
+  isAbsolutePathaFile,
+  isAbsolutePathaFolder,
+  verifyPathExtIsMD,
+  getFilesInFolder,
   getMDfilesFromArray,
+  getFileFromPathOrFolder,
 };
 module.exports = functions;
