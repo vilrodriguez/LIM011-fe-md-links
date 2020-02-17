2a4f3f9339cba1ff4a9a97634b7e243cdf6adcb1
Vil Rodriguez <vilmango@hotmail.com>
2020-02-17 17:04:50 -0500

    update refactoring of absolutepath functions and test, modularization of files

---
+++ src/validate.js
@@ -0,0 +1,47 @@
+const fetch = require('node-fetch');
+
+const verifyLinkStatus = (array) => {
+  let newObj = {};
+  const newArray = [];
+  array.forEach((element) => {
+    newArray.push(fetch(element.link).then((response) => {
+      const { status } = response;
+      const { statusText } = response;
+      if (status >= 200 && status <= 399) {
+        newObj = { ...element, status, message: statusText };
+      } else {
+        newObj = { ...element, status, message: 'Fail' };
+      }
+      return newObj;
+    }).catch((error) => {
+      console.log(error.message);
+      newObj = { ...element, message: 'Error: Invalid Link' };
+      return newObj;
+    }));
+  });
+  return Promise.all(newArray);
+};
+
+const stats = (obj) => {
+  const allLinks = obj.map((element) => element.link);
+  const links = allLinks.length;
+  const uniqueLinks = [...new Set(allLinks)].length;
+  return `Total Links in file: ${links} \nUnique Links: ${uniqueLinks}`;
+};
+
+const validateBrokenLinks = (obj) => {
+  const allLinks = obj.map((element) => element.link);
+  const links = allLinks.length;
+  const uniqueLinks = [...new Set(allLinks)].length;
+  const invalidLinks = obj.filter((element) => element.message === 'Fail');
+  const broken = invalidLinks.length;
+  return `Total Links in file: ${links} \nUnique Links: ${uniqueLinks} \nBroken: ${broken}`;
+};
+
+const validate = {
+  verifyLinkStatus,
+  validateBrokenLinks,
+  stats,
+};
+
+module.exports = validate;
