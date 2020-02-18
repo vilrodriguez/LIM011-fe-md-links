9f18daa03b47701255e3b37e880f3f79c106795f
Vil Rodriguez <vilmango@hotmail.com>
2020-02-18 18:40:14 -0500

    update mdlinks functions

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -1,15 +1,26 @@
 const functions = require('../src/index.js');
-const validate = require ('../src/validate.js');
+const validate = require('../src/validate.js');
 
-const mdlinks = (path, options) => {
-  new Promise((resolve) => {
-    if (options.validate === true) {
-      resolve(validate.verifyLinkStatus(path));
-    } else {
-      resolve(
+const mdlinks = (path, options) => new Promise((resolve) => {
+  if (options.validate === true) {
+    resolve(validate.verifyLinkStatus(path));
+  } else {
+    resolve(functions.returnLinks(path));
+  }
+});
 
-        returnLinks(arrayOfLinks, path),
-      );
-    }
-  });
-};
+module.exports = mdlinks;
+mdlinks('/home/vilmango/Documents/LIM011-fe-md-links/TestRead.md', { validate: false }).then((res) => console.log(res));
+
+// const mdlinks = (path, options) => {
+//   new Promise((resolve) => {
+//     if (options.validate === true) {
+//       resolve(validate.verifyLinkStatus(path));
+//     } else {
+//       resolve(
+
+//         returnLinks(arrayOfLinks, path),
+//       );
+//     }
+//   });
+// };
