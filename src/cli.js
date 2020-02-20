f656ccac5d616f5f0841c0f176a430341a80d2c9
Vil Rodriguez <vilmango@hotmail.com>
2020-02-20 17:40:28 -0500

    cli function and updated package jason for CLI commands

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+const cli = require('../src/mdlinksCli.js');
 
-// const cli = require('../src/mdlinksCli.js');
+const path = process.argv[2];
 
-// const path = process.argv[2];
+const option1 = process.argv[3];
 
-// const validate = process.argv[3];
+const option2 = process.argv[4];
 
-// const stat = process.argv[4];
+cli(path, option1, option2).then((response) => console.log(response));
+
+// console.log('aksdjalskjdklajdklas');
--- src/mdlinksCli.js
+++ src/mdlinksCli.js
@@ -1,41 +1,30 @@
 
 const mdlinks = require('../src/mdlinks.js');
 const validate = require('../src/validate.js');
+
 // option1 can be --validate and --stats, but option2 only --stats
 const cliFunction = (path, option1 /* option2 */) => {
   let finalResult;
   if (option1 === '--validate') {
     finalResult = mdlinks(path, { validate: true }).then((res) => {
+      let string = '';
       res.forEach((ele) => {
-        const string = `${ele.file} ${ele.link} ${ele.status} ${ele.message}`;
-      console.log(string);
-        return string;
+        string += `${ele.file} ${ele.link} ${ele.text} ${ele.message} ${ele.status} \n`;
       });
+      return string;
     });
   } else if (option1 === '--stats') {
     finalResult = mdlinks(path, { validate: true }).then((res) => (validate.stats(res)));
   } else if (option1 === null || option1 === undefined) {
-    finalResult = mdlinks(path, { validate: false }).then((res) => console.log(res));
+    finalResult = mdlinks(path, { validate: false }).then((res) => {
+      let string = '';
+      res.forEach((ele) => {
+        string += `${ele.file} ${ele.link} ${ele.text}\n`;
+      });
+      return string;
+    });
   }
   return finalResult;
 };
-console.log(cliFunction('/home/vilmango/Documents/LIM011-fe-md-links/TestRead.md', '--stats'));
-/*
-mdLinks("./some/example.md")
-  .then(links => {
-    // => [{ href, text, file }]
-  })
-  .catch(console.error);
 
-mdLinks("./some/example.md", { validate: true })
-  .then(links => {
-    // => [{ href, text, file, status, ok }]
-  })
-  .catch(console.error);
-
-mdLinks("./some/dir")
-  .then(links => {
-    // => [{ href, text, file }]
-  })
-  .catch(console.error);
-  */
+module.exports = cliFunction;
