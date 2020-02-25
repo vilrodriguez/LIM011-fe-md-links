be9d0040df5dc2896a29eed1d7b83e7787343c31
Vilmarys Rodriguez <vilmango@hotmail.com>
2020-02-24 22:38:28 -0500

    updated node to version 11 to resolve issue with flat() method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-debugger;
 const path = require('path');
 const fs = require('fs');
 
-const isAbsolutePathaFile = (filePath) => {
-//  console.log(filePath);
-  return fs.lstatSync(filePath).isFile();
-};
+const isAbsolutePathaFile = (filePath) => fs.lstatSync(filePath).isFile();
 const isAbsolutePathaFolder = (filePath) => fs.lstatSync(filePath).isDirectory();
 const getFilesInFolder = (filePath) => fs.readdirSync(filePath);
 const getMDfilesFromArray = (fileArray) => fileArray.filter((element) => path.extname(element) === '.md');
@@ -14,7 +10,7 @@ const resolveExistingPathToAbsolute = (route) => {
   let newPath;
   if (fs.existsSync(route)) {
     if (path.isAbsolute(route) === true) {
-      return route;
+      return (route);
     }
     newPath = path.resolve(route);
     return newPath;
@@ -22,26 +18,86 @@ const resolveExistingPathToAbsolute = (route) => {
   return 'Path does not exist';
 };
 const getFileFromPathOrFolder = (filePath) => {
-  console.log('>25', filePath);
   const files = resolveExistingPathToAbsolute(filePath);
   let arrayFiles = [];
   if (isAbsolutePathaFile(files) && path.extname(files) === '.md') {
     arrayFiles.push(files);
-    console.log('>30 ',arrayFiles);
   }
   if (isAbsolutePathaFolder(files)) {
     const folderFiles = getFilesInFolder(files);
-    console.log('>33', folderFiles);
     folderFiles.forEach((element) => {
       if (path.extname(element) === '.md') {
-        console.log('>34 element', element);
         arrayFiles = arrayFiles.concat(getFileFromPathOrFolder(path.join(filePath, element)));
-        console.log('>41 oaoaoaoa', arrayFiles)
       }
     });
   }
   return arrayFiles;
 };
+
+const readMdFile = (filesPath) => {
+  const string = fs.readFileSync(filesPath);
+  return string.toString();
+};
+
+const getLinksFromString = (stringFromFile) => stringFromFile.match(/\[(.+)\]\((.+)\)/gm);
+
+const returnLinks = (filePath) => {
+  const arrayOfFiles = getFileFromPathOrFolder(filePath);
+  const content = arrayOfFiles.map((ele) => {
+    const contentOfFile = readMdFile(ele);
+    const stringArray = getLinksFromString(contentOfFile);
+    return stringArray;
+  });
+  const newlinksArray = content.flat().map((element) => {
+    const url = element.match(/\((.+)\)/gm)[0];
+    const cleanLink = url.substring(1, url.length - 1);
+    const finalPath = resolveExistingPathToAbsolute(filePath);
+    return {
+      link: cleanLink,
+      text: element.match(/(\[[^\]]+\])/gm)[0],
+      file: finalPath,
+    };
+  });
+  return newlinksArray;
+};
+
+
+
+const functions = {
+  resolveExistingPathToAbsolute,
+  isAbsolutePathaFile,
+  isAbsolutePathaFolder,
+  getFilesInFolder,
+  getMDfilesFromArray,
+  getFileFromPathOrFolder,
+  getLinksFromString,
+  returnLinks,
+  readMdFile,
+};
+
+module.exports = functions;
+/* const returnLinks = (filePath) => {
+  console.log('>51 returnLinks ', filePath);
+  const arrayOfFiles = getFileFromPathOrFolder(filePath);
+  console.log('>53 array', arrayOfFiles);
+  const content = arrayOfFiles.map((ele) => {
+    const contentOfFile = readMdFile(ele);
+    const stringArray = getLinksFromString(contentOfFile);
+    return stringArray;
+  });
+  console.log('aaa', content);
+  const newlinksArray = content.flat().map((element) => {
+    const url = element.match(/\((.+)\)/gm)[0];
+    const cleanLink = url.substring(1, url.length - 1);
+    const finalPath = resolveExistingPathToAbsolute(filePath);
+    return {
+      link: cleanLink,
+      text: element.match(/(\[[^\]]+\])/gm)[0],
+      file: finalPath,
+    };
+  });
+  return newlinksArray;
+}; */
 
 /* const getFileFromPathOrFolder = (filePath) => {
   const files = resolveExistingPathToAbsolute(filePath);
@@ -65,49 +121,3 @@ const getFileFromPathOrFolder = (filePath) => {
   console.log('xoxoxox', arrayFiles);
   return arrayFiles;
 }; */
-
-const readMdFile = (filesPath) => {
-  const string = fs.readFileSync(filesPath);
-  return string.toString();
-};
-
-const getLinksFromString = (stringFromFile) => stringFromFile.match(/\[(.+)\]\((.+)\)/gm);
-
-const returnLinks = (filePath) => {
-  console.log('>51 returnLinks ', filePath);
-  const arrayOfFiles = getFileFromPathOrFolder(filePath);
-  console.log('>53 array', arrayOfFiles);
-  const content = arrayOfFiles.map((ele) => {
-    const contentOfFile = readMdFile(ele);
-    const stringArray = getLinksFromString(contentOfFile);
-    return stringArray;
-  });
-  console.log('aaa', content);
-  const newlinksArray = content.map((element) => {
-    const url = element.match(/\((.+)\)/gm)[0];
-    const cleanLink = url.substring(1, url.length - 1);
-    const finalPath = resolveExistingPathToAbsolute(filePath);
-    return {
-      link: cleanLink,
-      text: element.match(/(\[[^\]]+\])/gm)[0],
-      file: finalPath,
-    };
-  });
-  return newlinksArray;
-};
-
-console.log(returnLinks('TestRead.md'));
-
-const functions = {
-  resolveExistingPathToAbsolute,
-  isAbsolutePathaFile,
-  isAbsolutePathaFolder,
-  getFilesInFolder,
-  getMDfilesFromArray,
-  getFileFromPathOrFolder,
-  getLinksFromString,
-  returnLinks,
-  readMdFile,
-};
-
-module.exports = functions;
